ee066c0f7468d3cbc018bea1fdd1291fbe315f85
julioephs <116284308+julioephs@users.noreply.github.com>
2022-12-05 10:29:30 -0600

    Add files via upload

--- studentProjects/emma.js
+++ studentProjects/emma.js
@@ -1,34 +1,34 @@
 
-let louisa = function(processingInstance) {
+let emma = function(processingInstance) {
     with (processingInstance) {
      size(400, 400); 
      frameRate(30);
 
 //*****STUDENT CODE BELOW(((((((*******
 
-var myName = "Louisa";
-var holidayMessage = "Merry Christmas";
+
+var myName = "Emms";
+var holidayMessage = "    Marry Christmas ";
 
 
 
 //**************SKY*(*******************
-background(135, 243, 255); //sets sky to blue
+background(242, 27, 41); //sets sky to blue
 
 
 
 //**************FLOOR*******************
-fill(255,255,255); //sets floor to white
+fill(37, 161, 122); //sets floor to white
 rect(-27,300,430,321);
 
 
 
 //**************TREE*******************
-
-//add a fill below for tree stub color
-fill(115, 22, 22);
-rect(175,300,50,75,80); //Tree Stub
-//add a fill below for tree color
-fill(39, 161, 85);
+//Tree Stub 
+fill (130, 30, 30); 
+rect(175,300,50,75,80);
+//draw tree
+fill(12, 128, 21); //color tree
 triangle(50,325, 350,325, 200, 50);
 triangle(75,250, 325,250, 200, 50);
 triangle(100,150, 300,150, 200, 30);
@@ -36,38 +36,34 @@ triangle(100,150, 300,150, 200, 30);
 //tree top decoration
 //REPLACE MY DINOSAUR WITH A STAR
 // image(getImage("cute/Star"), 180, 4, 40,48); 
-// image(getImage("cute/EnemyBug"), 180,200,40,50);
 
 
 
 //**************ORNAMENT ONE*******************
 //ADD FILL
-fill(179, 46, 46);
-ellipse(120,288, 50,50);
+fill(224, 18, 18);
+ellipse(119,288, 50,50);
 arc(120,288,50,20,0,180);
 arc(120,280,50,20,0,180);
 
 //**************ORNAMENT TWO*******************
 //ADD FILL
-fill(0, 255, 102);
+fill (0, 255, 89);
 ellipse(250,188, 50,50);
 arc(250,188,50,20,0,180);
 arc(250,180,50,20,0,180);
 
 //**************ORNAMENT THREE*******************
 //ADD FILL
-fill(119, 131, 224);
-ellipse(281,285, 50,50);
+fill (38, 94, 235);
+ellipse(281,284, 50,50);
 arc(281,285,50,20,0,180);
 arc(281,275,50,20,0,180);
 
-//Personal Ornament
-fill(233, 235, 181);
-ellipse(200,100,50,50);
-fill(204, 38, 157);
-ellipse(150,200,50,50);
-
-
+//*************ORNAMENT FOUR*******************
+fill (173, 45, 156);
+ellipse(151,188, 49,48);
+arc(151, 189, 50, 20,0,180);
 //***************TEXT***************************
 //setup font and black text
 var fontChosen = createFont("cursive"); //setups cursive font
@@ -80,12 +76,6 @@ text("Made by " + myName, 250, 375, 200, 100);
 //display "message" text and set font size to 30
 textFont(fontChosen,30);
 text(holidayMessage, 115, 25, 200, 100);
-
-
-
-
-
-
 
 
    }};
